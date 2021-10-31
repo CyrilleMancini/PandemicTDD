c116aca5cfbcfc0a147e57acc442134e5a5898a5
Cyrille Mancini <cyrille.mancini@devologeek.net>
2021-10-31 20:36:54 +0100

    Ajoute les 48 Villes du plateau.

--- PandemicTDD/Materiel/Board.cs
+++ PandemicTDD/Materiel/Board.cs
@@ -1,9 +1,29 @@
-﻿namespace PandemicTDD.Materiel
+﻿using System;
+using System.Collections.Generic;
+
+namespace PandemicTDD.Materiel
 {
     public class Board
     {
         public Board()
         {
+            InitTowns();
         }
+
+        private void InitTowns()
+        {
+            Towns = new List<Town>() {
+            new Town(), new Town(), new Town(), new Town(), new Town(), new Town(), new Town(),
+            new Town(), new Town(), new Town(), new Town(), new Town(), new Town(), new Town(),
+            new Town(), new Town(), new Town(), new Town(), new Town(), new Town(), new Town(),
+            new Town(), new Town(), new Town(), new Town(), new Town(), new Town(), new Town(),
+            new Town(), new Town(), new Town(), new Town(), new Town(), new Town(), new Town(),
+            new Town(), new Town(), new Town(), new Town(), new Town(), new Town(), new Town(),
+            new Town(), new Town(), new Town(), new Town(), new Town(), new Town(),
+
+            };
+        }
+
+        public List<Town> Towns { get; private set; }
     }
 }
--- PandemicTDDTests/Materiel/BoardTest.cs
+++ PandemicTDDTests/Materiel/BoardTest.cs
@@ -15,5 +15,13 @@ namespace PandemicTDDTests.Materiel
             Assert.IsNotNull(Board);
         }
 
+        [TestMethod()]
+        public void CheckExpectedTownNumberTest()
+        {
+            Board Board = GameBox.GetBoard();
+            Assert.AreEqual(48,Board.Towns.Count, "Le plateau doit contenir 48 villes");
+        }
+
+
     }
 }
