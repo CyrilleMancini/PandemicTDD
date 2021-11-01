464d1a79c9a73192db56840b535c62a6963c0f4b
Cyrille Mancini <cyrille.mancini@devologeek.net>
2021-11-01 21:23:50 +0100

    Ajoute la fonctionnalité Reset()

--- PandemicTDD/Materiel/SpreadIndicator.cs
+++ PandemicTDD/Materiel/SpreadIndicator.cs
@@ -5,18 +5,30 @@ namespace PandemicTDD.Materiel
     public class SpreadIndicator
     {
         public int CurrentLevel { get; set; }
-        
-        public int SpreadSpeed { get; set; }
+
+        public int SpreadSpeed
+        {
+            get
+            {
+                return Levels[CurrentLevel - 1];
+            }
+        }
+
+        int[] Levels = new[] { 2, 2, 2, 3, 3, 4, 4 };
 
         public SpreadIndicator()
         {
             CurrentLevel = 1;
-            SpreadSpeed = 2;
         }
 
         public void Next()
         {
             CurrentLevel++;
+        }
+
+        public void Reset()
+        {
+            CurrentLevel = 1;
         }
     }
 }
--- PandemicTDDTests/Materiel/SpreadIndicatorTests.cs
+++ PandemicTDDTests/Materiel/SpreadIndicatorTests.cs
@@ -31,13 +31,51 @@ namespace PandemicTDDTests.Materiel
         }
 
         [TestMethod()]
-        public void GoToNextSpreadSpeedTest()
+        public void ResetSpreadSpeedTest()
         {
             Board Board = GameBox.GetBoard();
             SpreadIndicator si = Board.GetSpreadIndicator();
             si.Next();
+            si.Next();
+
+            si.Reset();
+            Assert.AreEqual(1, si.CurrentLevel);
+        }
+
+        [TestMethod()]
+        public void GoToNextSpreadSpeedTest()
+        {
+            Board Board = GameBox.GetBoard();
+            SpreadIndicator si = Board.GetSpreadIndicator();
+            si.Reset();
+            si.Next();
             Assert.AreEqual(2, si.CurrentLevel);
         }
 
+        [TestMethod()]
+        public void GoFinalSpreadLevelTest()
+        {
+            Board Board = GameBox.GetBoard();
+            SpreadIndicator si = Board.GetSpreadIndicator();
+            si.Reset();
+
+            for (int i = 0; i < 6; i++)
+                si.Next();
+                        
+            Assert.AreEqual(7, si.CurrentLevel);
+        }
+
+        [TestMethod()]
+        public void GoFinalSpreadSpeedTest()
+        {
+            Board Board = GameBox.GetBoard();
+            SpreadIndicator si = Board.GetSpreadIndicator();
+            si.Reset();
+
+            for (int i = 0; i < 6; i++)
+                si.Next();
+            
+            Assert.AreEqual(4, si.SpreadSpeed);
+        }
     }
 }
