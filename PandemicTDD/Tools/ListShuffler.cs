d91659b50dfbaac99cdf6a4aa9b9b55e98ef6f4d
Cyrille Mancini <cyrille.mancini@devologeek.net>
2021-11-12 21:10:45 +0100

    Ajoute un Mélangeur de carte.

---
+++ PandemicTDD/Tools/ListShuffler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PandemicTDD.Tools
+{
+    public class ListShuffler
+    {
+        public ListShuffler()
+        {
+        }
+
+        Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+        internal List<T> Shuffle<T>(List<T> list)
+        {
+            List<T> shuffled = new List<T>();
+            while (list.Count != 0)
+            {
+                int idx = rnd.Next(0, list.Count);
+                shuffled.Add(list[idx]);
+                list.RemoveAt(idx);
+            }
+            return shuffled;
+        }
+    }
+}
