bdc54aa164bdf2c4ac1848e874c9b7dd7c67a5b9
Cyrille Mancini <cyrille.mancini@devologeek.net>
2021-11-14 21:03:46 +0100

    Ajoute  l'action de vol charter.

---
+++ PandemicTDD/Actions/CharterFlightAction.cs
@@ -0,0 +1,51 @@
+﻿using PandemicTDD;
+using PandemicTDD.Materiel;
+using PandemicTDD.Materiel.PlayerCards;
+using System;
+
+namespace PandemicTDD.Actions
+{
+    internal class CharterFlightAction : ActionBase
+    {
+        private GameState gameState;
+        private string Destination;
+        private TownSlot PlayerSlotTown;
+        private TownSlot DestSlotTown;
+        private PlayerTownCard OwnedCityCard;
+
+        public CharterFlightAction(GameState gameState, string destination)
+        {
+            this.gameState = gameState;
+            this.Destination = destination;
+        }
+
+        public override void Execute()
+        {
+
+            gameState.CurrentPlayer.Town = DestSlotTown.Town;
+
+            gameState.CurrentPlayer.PlayerCards.Remove(OwnedCityCard);
+            gameState.Board.PlayerDiscardCardStack.Push(OwnedCityCard);
+
+        }
+
+        public override void Try()
+        {
+            PlayerSlotTown = gameState.Board.GetTownSlot(gameState.CurrentPlayer.Town.Name);
+            DestSlotTown = gameState.Board.GetTownSlot(Destination);
+
+            if (PlayerSlotTown.Town.Name == Destination)
+                throw new ArgumentException(DriveFerryAction.ErrorSameDestinationAndLocation);
+
+            try
+            {
+                OwnedCityCard = (PlayerTownCard)gameState.CurrentPlayer.GetCityPlayerCard(PlayerSlotTown.Town.Name);
+            }
+            catch (NotOwnedCityPlayerCardException)
+            {
+                throw new NotOwnedCityPlayerCardException($"You must own origine {PlayerSlotTown.Town.Name} card to do a charter Flight");
+            }
+
+        }
+    }
+}
---
+++ PandemicTDDTests/Running/Actions/CharterFlightActionTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PandemicTDD;
+using PandemicTDD.Actions;
+using PandemicTDD.Materiel.Initializers;
+using PandemicTDD.Materiel.PlayerCards;
+using PandemicTDDTests.Materiel;
+using System.Linq;
+
+namespace PandemicTDDTests.Running.Actions
+{
+    [TestClass]
+    public class CharterFlightActionTest : TestsBase
+    {
+
+        [TestMethod]
+        public void CharterFlightFromOwnedTownCard()
+        {
+            StartGame();
+
+            PlayerCard OrigineCard = GameBox.GetPlayersCard().First(c => c is PlayerTownCard ct && ct.Town.Name == TownsInitializer.Atlanta);
+            Players[0].PlayerCards.Add(OrigineCard);
+
+            ActionBase action = new CharterFlightAction(GameState, TownsInitializer.Paris);
+            GameState.DoAction(action);
+
+            Assert.AreEqual(TownsInitializer.Paris, GameState.CurrentPlayer.Town.Name);
+            Assert.AreEqual(3, GameState.ActionsRemaining);
+        }
+
+        [TestMethod]
+        public void CharterFlightToNotOwnedTownCard()
+        {
+            StartGame();
+            GameState.CurrentPlayer.PlayerCards.Clear();
+
+            ActionBase action = new CharterFlightAction(GameState, TownsInitializer.Paris);
+            Assert.ThrowsException<NotOwnedCityPlayerCardException>(() =>
+            {
+                action.Try();
+                action.Execute();
+            });
+            Assert.AreEqual(4, GameState.ActionsRemaining);
+            Assert.AreEqual(TownsInitializer.Atlanta, GameState.CurrentPlayer.Town.Name);
+
+        }
+
+        [TestMethod]
+        public void UsedCardIsOnTopOfPayerCard()
+        {
+            StartGame();
+
+            PlayerCard OrigineCard = GameBox.GetPlayersCard().First(c => c is PlayerTownCard ct && ct.Town.Name == TownsInitializer.Atlanta);
+            Players[0].PlayerCards.Add(OrigineCard);
+
+            ActionBase action = new CharterFlightAction(GameState, TownsInitializer.Paris);
+            GameState.DoAction(action);
+
+
+            Assert.AreEqual(OrigineCard, GameState.Board.PlayerDiscardCardStack.Peek(), "Player card shoud be in Discard stack");
+            Assert.ThrowsException<NotOwnedCityPlayerCardException>(() =>
+            {
+                OrigineCard = GameState.CurrentPlayer.GetCityPlayerCard(TownsInitializer.Paris);
+            }, "Player shouldn't have the used card anymore");
+
+        }
+
+    }
+}
