c6804ff1119b5dda39d9625caaf6211b13944a95
Cyrille Mancini <cyrille.mancini@devologeek.net>
2021-11-28 19:34:29 +0100

    Ajoute le test de decouverte de remede en 4 cartes au lieu de 5

--- PandemicTDDTests/Running/RolesActions/ScientistActionsTests.cs
+++ PandemicTDDTests/Running/RolesActions/ScientistActionsTests.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PandemicTDD.Actions;
 using PandemicTDD.Materiel;
+using PandemicTDD.Materiel.PlayerCards;
+using PandemicTDDTests.Running.Actions;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PandemicTDDTests.Materiel
 {
@@ -10,8 +15,26 @@ namespace PandemicTDDTests.Materiel
         public void InitPlayer()
         {
             StartGame();
-            Players[0].Role = new ScientistRoleCard("Scientifique");
-            Players[1].Role = new DispatcherRoleCard("Répartiteur");
+            Players[0].Role = new ScientistRoleCard("Scientifique");      
+        }
+
+
+        [TestMethod]
+        public void OnlyFourCardsNeededForScientist()
+        {
+            List<PlayerTownCard> cards = new();
+            for (int c = 0; c < 4; c++)
+                cards.Add((PlayerTownCard)GameBox.GetPlayersCard().First(c => c is PlayerTownCard ct && ct.Town.Color == DiseaseColor.Red));
+
+            ActionBase action = new DiscoverCureAction(GameState, DiseaseColor.Red, cards);
+            GameState.DoAction(action);
+
+            Assert.AreEqual(DiseaseStatus.Treated, GameState.Board.GetCureSlots().RedSlot.Status);
+
+            Assert.AreEqual(cards[3], GameState.Board.PlayerDiscardCardStack.Pop());
+            Assert.AreEqual(cards[2], GameState.Board.PlayerDiscardCardStack.Pop());
+            Assert.AreEqual(cards[1], GameState.Board.PlayerDiscardCardStack.Pop());
+            Assert.AreEqual(cards[0], GameState.Board.PlayerDiscardCardStack.Pop());
         }
     }
 }
